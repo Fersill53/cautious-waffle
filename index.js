f6ceb5e37cb73ad19ae48984dcb3ae15ee23d306
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-06-25 16:41:09 -0500

    added basic html

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const inquirer = require('inquirer');
 
 const fs = require('fs-extra');
+
+const generateHTML = (data) => {
+    const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>${data.title}</title>
+      </head>
+      <body>
+        <h1>${data.name}</h1>
+        <h2>${data.title}</h2>
+        <h2>${data.description}</h2>
+        <h2>${data.installation}</h2>
+        <h2>${data.usage}</h2>
+        <h2>${data.contribution}</h2>
+        <h2>${data.github}</h2>
+      </body>
+    </html>
+  `;
+
+  return htmlContent;
+};
+
