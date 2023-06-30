7aa3c645a5c62948deebaf1e2d98781322291467
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-06-29 20:22:11 -0500

    simplified code

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# cautious-waffle
-README generator from user input to prompts
+
+# test
+
+## Description
+test
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+test
+
+## Usage
+test
+
+## Contributing
+test
+
+## Tests
+test
+
+## License
+This project is licensed under the MIT license.
+
+## Questions
+For any questions or inquiries, please contact me:
+- GitHub: [test](https://github.com/test)
+- Email: test
--- index.js
+++ index.js
@@ -1,92 +1,188 @@
+// const inquirer = require('inquirer');
+
+// const fs = require('fs-extra');
+
+// const generateHTML = (data) => {
+//     const htmlContent = `
+//     <!DOCTYPE html>
+//     <html>
+//       <head>
+//         <title>${data.title}</title>
+//       </head>
+//       <body>
+//         <h1>${data.name}</h1>
+//         <h2>${data.title}</h2>
+//         <h2>${data.description}</h2>
+//         <h2>${data.installation}</h2>
+//         <h2>${data.usage}</h2>
+//         <h2>${data.contribution}</h2>
+//         <h2>${data.github}</h2>
+//       </body>
+//     </html>
+//   `;
+
+//   return htmlContent;
+// };
+
+// const promptUser = async() => {
+//     try {
+//         const answers = await inquirer.prompt ([
+    
+//         {
+//             type: 'input',
+//             name: 'title',
+//             message: 'Enter project title',
+
+//         },
+
+//         {
+//             type: 'input',
+//             name: 'description',
+//             message: 'Enter project description',
+//         },
+
+//         {
+//             type: 'input',
+//             name: 'installation',
+//             message: 'Enter steps for installation',
+//         },
+
+//         {
+//             type: 'input',
+//             name: 'usage',
+//             message: 'Describe what project will be used for',
+//         },
+
+//         {
+//             type:'input',
+//             name: 'contribution',
+//             message: 'Enter contribution info',
+//         },
+
+//         {
+//             type: 'input',
+//             name: 'github',
+//             message: 'For questions (github)'
+//         },
+
+//         {
+//             type: 'list',
+//             name: 'license',
+//             message: 'Type of license used',
+//             choices: ['None', 'ISC', 'MIT'],
+//         },
+//     ]);
+
+
+//         console.log(answers);
+//         return answers;}
+
+//         catch (error) {
+//             console.log(error);
+        
+//         }
+//         };
+
+//         promptUser();
+
+const fs = require('fs');
 const inquirer = require('inquirer');
 
-const fs = require('fs-extra');
+// Prompt questions using Inquirer
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of your project: ',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project: ',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter the installation instructions: ',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information: ',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contribution guidelines: ',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions: ',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Enter the project license: ',
+      choices: ['None', 'ISC', 'MIT']
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your GitHub username: ',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address: ',
+    },
+  ])
+  .then((answers) => {
+    const readmeContent = generateReadme(answers);
+    fs.writeFile('README.md', readmeContent, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README.md file successfully generated!');
+      }
+    });
+  });
 
-const generateHTML = (data) => {
-    const htmlContent = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>${data.title}</title>
-      </head>
-      <body>
-        <h1>${data.name}</h1>
-        <h2>${data.title}</h2>
-        <h2>${data.description}</h2>
-        <h2>${data.installation}</h2>
-        <h2>${data.usage}</h2>
-        <h2>${data.contribution}</h2>
-        <h2>${data.github}</h2>
-      </body>
-    </html>
-  `;
+// Function to generate the README content
+function generateReadme(answers) {
+  return `
+# ${answers.title}
 
-  return htmlContent;
-};
+## Description
+${answers.description}
 
-const promptUser = async() => {
-    try {
-        const answers = await inquirer.prompt ([
-    
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Enter project title',
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
 
-        },
+## Installation
+${answers.installation}
 
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter project description',
-        },
+## Usage
+${answers.usage}
 
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Enter steps for installation',
-        },
+## Contributing
+${answers.contributing}
 
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Describe what project will be used for',
-        },
+## Tests
+${answers.tests}
 
-        {
-            type:'input',
-            name: 'contribution',
-            message: 'Enter contribution info',
-        },
+## License
+This project is licensed under the ${answers.license} license.
 
-        {
-            type: 'input',
-            name: 'github',
-            message: 'For questions (github)'
-        },
-
-        {
-            type: 'list',
-            name: 'license',
-            message: 'Type of license used',
-            choices: ['None', 'ISC', 'MIT'],
-        },
-    ]);
-
-
-        console.log(answers);
-        return answers;}
-
-        catch (error) {
-            console.log(error);
-        
-
-        //function runQuery() {
-        //return inquirer.prompt(questions)
-        //.then((answers) =>{
-            //console.log(answers)
-           // return answers
-        }
-        };
-
-        promptUser();
+## Questions
+For any questions or inquiries, please contact me:
+- GitHub: [${answers.username}](https://github.com/${answers.username})
+- Email: ${answers.email}
+`;
+}
